c68c419960db80b8d4fa0753d492d85422e0161e
Shengyu Zhang <i@silverrainz.me>
2022-04-14 00:38:22 +0800

    Bump version

--- sphinxnotes/lilypond/__init__.py
+++ sphinxnotes/lilypond/__init__.py
@@ -29,7 +29,7 @@ from . import lilypond
 
 __title__= 'sphinxnotes-lilypond'
 __license__ = 'BSD'
-__version__ = '1.5'
+__version__ = '1.5.1'
 __author__ = 'Shengyu Zhang'
 __url__ = 'https://sphinx-notes.github.io/lilypond'
 __description__ = 'Sphinx extension for Lilypond'
